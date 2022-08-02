d6c7ef089034d2a9afb73c9d980a60c8fb55cf45
Dmitriy <dmitriy.bazhenov94@gmail.com>
2022-08-02 17:13:38 +0300

    validate

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-// const anchors = document.querySelectorAll('.scroll');
-
-
-// // плавный скролл
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     closeModal();
-//     const blockID = anchor.getAttribute('href').substr(1);
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
-
 document.querySelectorAll('.scroll').forEach(link => {
 
   link.addEventListener('click', function(e) {
@@ -26,8 +8,7 @@ document.querySelectorAll('.scroll').forEach(link => {
 
       const scrollTarget = document.getElementById(href);
 
-      // const topOffset = document.querySelector('.scrollto').offsetHeight;
-      const topOffset = 70; // если не нужен отступ сверху 
+      const topOffset = 70;
       const elementPosition = scrollTarget.getBoundingClientRect().top;
       const offsetPosition = elementPosition - topOffset;
 
